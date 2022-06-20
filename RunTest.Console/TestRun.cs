055939ec172c97829e663c42a0013a9142c0e6fe
Claudio Vinicius da Silva <claudio.silva@sequor.com.br>
2022-06-20 15:52:08 -0300

    Chore: Implementa o cliente OPC através da biblioteca Opc.Ua.Fx
    Objetivo do commit: Commitar as dependencias

---
+++ RunTest.Console/TestRun.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using OpcUaCommon;
+
+namespace System
+{
+    class TestRun
+    {
+        static void Main(string[] args)
+        {            
+            ClassOPCClient client = null;
+            try
+            {
+                client = new ClassOPCClient("opc.tcp://SQO-053.energpower.com.br:4840");
+                client.SetNodeValue("ns=3;i=1013", "102");
+            }
+            catch(Exception Error)
+            {
+               
+                Console.WriteLine(Error.Message);
+            }
+
+            var a = client.GetNodeValue("ns=3;i=1009");
+            Console.WriteLine(a.ToString());
+            
+        }
+    }
+}
